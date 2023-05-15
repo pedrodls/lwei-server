dce8279308181bc9767972fa11811c47e42fdc54
= <=>
2023-05-15 19:26:24 +0100

    Fix controller of googleAuth

--- providers/google/sreadsheets.service.js
+++ providers/google/sreadsheets.service.js
@@ -1,55 +1,71 @@
 const { google } = require("googleapis");
 
-
 exports.getMetaData = async (req, res, next) => {
-
   const auth = new google.auth.GoogleAuth({
-    keyFile:'credentials.json',
+    keyFile: "credentials.json",
     scopes: [process.env.SCOPES],
   });
 
   const sheets = google.sheets({ version: "v4", auth });
-  
+
   const values = await sheets.spreadsheets.values.get({
     auth,
     spreadsheetId: process.env.SPREADSHEETS_API,
-    range: 'ordens'
-  })
+    range: "ordens",
+  });
 
-const dataModel = {
-  "id_negociacao": '',
-  "titulo": '',
-  "valor_nominal": '',
-  "taxa_cupao": '',
-  "data_emissao": '',
-  "data_maturidade": '',
-  "ultima_cotacao": '',
-  "compra": {
-    "Quantidade": '',
-    "Preço": ''
-  },
-  "venda": {
-    "Quantidade": '',
-        "Preço": ''
-  }
-} 
+  const dataModel = {
+    id_negociation: "",
+    title_type: "",
+    nominal_value: "",
+    cupon: "",
+    createdAt: "",
+    maturity_date: "",
+    last_cotation: "",
+    shop: {
+      amount: "",
+      price: "",
+    },
+    sell: {
+      amount: "",
+      price: "",
+    },
+  };
 
+  const data_list = [];
 
-for (let i = 0; i < values.data.values.length; i++) {
-  
-  if(i > 1){
-    if(values.data.values[i].length > 0){
-      const data = values.data.values[i];
+  for (let i = 2; i < values.data.values.length; i++) {
 
-      dataModel.id_negociacao = data[0]
-      dataModel.titulo = data[1]
+    const data = values.data.values[i];
 
-      console.log(dataModel);
+    if (values.data.values[i].length > 0) {
+
+      dataModel.id_negociation = data[0].toString().trim();
+
+      dataModel.title_type = data[1];
+
+      dataModel.nominal_value = data[2];
+
+      dataModel.cupon = data[3];
+
+      dataModel.createdAt = data[4];
+
+      dataModel.maturity_date = data[5];
+
+      dataModel.last_cotation = data[6];
+
+      dataModel.shop.price = data[7];
+
+      dataModel.shop.amount = data[8];
+
+      dataModel.sell.price = data[9];
+
+      dataModel.sell.amount = data[10];
+
+      data_list.push(dataModel);
 
     }
   }
-  
-}
-  res.status(200).send(values.data.values);
 
+  res.status(200).send(data_list);
 };
